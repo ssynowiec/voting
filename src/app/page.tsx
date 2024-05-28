15ee24589d1704cabf7afbe5021b0bce5f5ff69b
Stanisław Synowiec <stasyn1410@gmail.com>
2024-05-28 22:10:38 +0200

    add: components
    install shadcn/ui components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from '@/components/ui/button';
+
 const Home = () => {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
 			<h1>Next.js Template</h1>
 			<p>Start template with tailwind, prettier & eslint</p>
+			<Button>Click me</Button>
 		</main>
 	);
 };
