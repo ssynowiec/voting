45f37b1bc658a1589556378e0a63bc5b2267357c
Stanisław Synowiec <stasyn1410@gmail.com>
2024-05-29 18:57:14 +0200

    chore: remove radix & react 19 ref warning from console.log

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,19 @@ import createJiti from 'jiti';
 const jiti = createJiti(fileURLToPath(import.meta.url));
 jiti('./src/env');
 
+const messageToIgnore = 'Warning: Accessing element.ref was removed in React 19. ref is now a regular prop.';
+const originalError = console.error;
+
+console.error = function(text, ...args) {
+	if (typeof text === 'string' && text.includes(messageToIgnore)) {
+		return;
+	}
+
+	return originalError.call(this, text, ...args);
+};
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
 export default nextConfig;
+
