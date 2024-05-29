b5f5da7ec7016fdda31cf1b0f584f6ac781d3143
Stanisław Synowiec <stasyn1410@gmail.com>
2024-05-29 20:46:45 +0200

    style: center join form

--- src/app/join/page.tsx
+++ src/app/join/page.tsx
@@ -2,9 +2,9 @@ import { OTPForm } from '@/components/OTPForm';
 
 const JoinPage = () => {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<h1 className="text-3xl font-bold">Join</h1>
+		<main className="flex min-h-screen flex-col items-center justify-center p-24">
 			<section className="flex flex-col items-center justify-center gap-4">
+				<h1 className="text-3xl font-bold">Join</h1>
 				<OTPForm />
 			</section>
 		</main>
