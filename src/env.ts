99d14c6bdabfa68aa8aff68f7562d6f3d9db7824
Stanisław Synowiec <stasyn1410@gmail.com>
2024-06-04 20:44:31 +0200

    chore: add env WS url

--- src/env.ts
+++ src/env.ts
@@ -4,9 +4,11 @@ import { z } from 'zod';
 export const env = createEnv({
 	client: {
 		NEXT_PUBLIC_API_URL: z.string().min(1),
+		NEXT_PUBLIC_WS_URL: z.string().min(1),
 	},
 	runtimeEnv: {
 		NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+		NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL,
 	},
 	emptyStringAsUndefined: true,
 });
