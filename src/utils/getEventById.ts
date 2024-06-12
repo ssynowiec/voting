0a001d1de198796c23d7ee6016e88475eb228dcf
Stanisław Synowiec <stasyn1410@gmail.com>
2024-06-12 10:28:51 +0200

    type: add zod parse fetch

--- src/utils/getEventById.ts
+++ src/utils/getEventById.ts
@@ -1,4 +1,27 @@
 import { env } from '@/env';
+import { z } from 'zod';
+
+const statusEnum = z.enum(['active', 'inactive']);
+const progressEnum = z.enum(['not-started', 'in-progress', 'completed']);
+
+const literalSchema = z.union([z.string(), z.number(), z.boolean(), z.null()]);
+type Literal = z.infer<typeof literalSchema>;
+type Json = Literal | { [key: string]: Json } | Json[];
+const jsonSchema: z.ZodType<Json> = z.lazy(() =>
+	z.union([literalSchema, z.array(jsonSchema), z.record(jsonSchema)]),
+);
+
+const eventSchema = z.object({
+	id: z.string().uuid(),
+	name: z.string(),
+	description: z.string(),
+	question: z.string(),
+	answers: jsonSchema,
+	status: statusEnum,
+	createdAt: z.string(),
+	eventCode: z.string(),
+	progress: progressEnum,
+});
 
 export const getEventById = async (id: string) => {
 	const res = await fetch(`${env.NEXT_PUBLIC_API_URL}/event/${id}`);
@@ -7,5 +30,5 @@ export const getEventById = async (id: string) => {
 		return undefined;
 	}
 
-	return await res.json();
+	return eventSchema.parse(await res.json());
 };
