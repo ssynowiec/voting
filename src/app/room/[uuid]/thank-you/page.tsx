93e30ed3861a106c9b076986bb5346b4b1e9b4f1
Stanisław Synowiec <stasyn1410@gmail.com>
2024-06-12 08:52:14 +0200

    add: simple thank you page

---
+++ src/app/room/[uuid]/thank-you/page.tsx
@@ -0,0 +1,11 @@
+const ThankYouPage = () => {
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-center p-24">
+			<section className="flex flex-col items-center justify-center gap-4">
+				<h1 className="text-3xl font-bold">Thank you for your vote</h1>
+			</section>
+		</main>
+	);
+};
+
+export default ThankYouPage;
