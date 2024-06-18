31fc7fa22fdcd0d3494111c600d6ebed274ff88c
Stanisław Synowiec <stasyn1410@gmail.com>
2024-06-18 14:50:55 +0200

    chore: enable experimental features in next

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,13 @@ const jiti = createJiti(fileURLToPath(import.meta.url));
 jiti('./src/env');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	experimental: {
+		reactCompiler: true,
+		ppr: 'incremental',
+		after: true,
+	},
+};
 
 export default nextConfig;
 
